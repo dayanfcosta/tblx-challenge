8f76e1f55174dd3781094c8b8105d1e306387986
Dayan Costa <dayan.costa@trustly.com>
2020-04-17 18:43:33 +0100

    Initial commit

---
+++ src/test/java/com/dayanfcosta/tblx/challenge/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dayanfcosta.tblx.challenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
