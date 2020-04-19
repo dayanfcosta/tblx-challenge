a96652938b487e15196f1eced1532e09d9a8ab96
Dayan Costa <dayan.costa@trustly.com>
2020-04-19 22:28:36 +0100

    Finishing the architecture using docker images
    Former-commit-id: 06c613ab8c559a8de3d8286579629b5703922402

--- data/import.sh
+++
@@ -1,6 +0,0 @@
-#! /bin/bash
-
-mongoimport --type csv --db gps-api --collection gps-data --host mongodb \
-  --columnsHaveTypes \
-  --fields="timestamp.auto(),lineId.auto(),direction.auto(),journeyPatternId.auto(),timeFrame.date(2006-01-02),vehicleJourneyId.auto(),operator.auto(),congestion.boolean(),longitude.double(),latitude.auto(),delay.auto(),blockId.auto(),vehicleId.auto(),stopId.auto(),atStop.boolean()" \
-  --file /data/siri.20121106.csv
