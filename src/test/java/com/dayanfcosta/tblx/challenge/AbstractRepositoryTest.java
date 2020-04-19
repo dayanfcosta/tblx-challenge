b3c7faff7df1325384ed67dea56f6308b0fa5620
Dayan Costa <dayan.costa@trustly.com>
2020-04-19 13:26:46 +0100

    Refactoring test classes to became more reusable

---
+++ src/test/java/com/dayanfcosta/tblx/challenge/AbstractRepositoryTest.java
@@ -0,0 +1,63 @@
+package com.dayanfcosta.tblx.challenge;
+
+import static java.time.LocalDate.now;
+import static java.util.stream.Collectors.toList;
+
+import java.util.stream.IntStream;
+import org.bson.Document;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.data.mongo.DataMongoTest;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+@DataMongoTest
+@ActiveProfiles("test")
+@ExtendWith(SpringExtension.class)
+public abstract class AbstractRepositoryTest {
+
+  @Autowired
+  private MongoTemplate mongoTemplate;
+
+  @BeforeEach
+  public void setUp() {
+    insertDocuments();
+  }
+
+  @AfterEach
+  void tearDown() {
+    mongoTemplate.dropCollection("gps-data");
+  }
+
+  protected MongoTemplate getMongoTemplate() {
+    return mongoTemplate;
+  }
+
+  private void insertDocuments() {
+    final var documents = IntStream.rangeClosed(0, 10).mapToObj(this::createDocument).collect(toList());
+    getMongoTemplate().insert(documents, "gps-data");
+  }
+
+  private Document createDocument(final int documentNumber) {
+    return new Document()
+        .append("timestamp", 1352246396000000L + documentNumber)
+        .append("lineId", documentNumber)
+        .append("direction", "1")
+        .append("journeyPatternId", documentNumber)
+        .append("timeFrame", now().plusDays(documentNumber))
+        .append("vehicleJourneyId", documentNumber)
+        .append("operator", "OPERATOR")
+        .append("congestion", false)
+        .append("longitude", -6.258584)
+        .append("latitude", 53.340099)
+        .append("delay", 0)
+        .append("blockId", documentNumber)
+        .append("vehicleId", documentNumber)
+        .append("stopId", documentNumber)
+        .append("atStop", false);
+  }
+
+}
--- src/test/java/com/dayanfcosta/tblx/challenge/operator/OperatorRepositoryTest.java
+++ src/test/java/com/dayanfcosta/tblx/challenge/operator/OperatorRepositoryTest.java
@@ -1,25 +1,22 @@
 package com.dayanfcosta.tblx.challenge.operator;
 
 import static java.time.LocalDate.now;
-import static java.util.stream.Collectors.toList;
 import static org.assertj.core.api.Assertions.assertThat;
 
-import com.dayanfcosta.tblx.challenge.AbstractSpringTest;
-import java.util.stream.IntStream;
-import org.bson.Document;
+import com.dayanfcosta.tblx.challenge.AbstractRepositoryTest;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-class OperatorRepositoryTest extends AbstractSpringTest {
+class OperatorRepositoryTest extends AbstractRepositoryTest {
 
-  private static final String OPERATOR = "XPTO";
+  private static final String OPERATOR = "OPERATOR";
 
   private OperatorRepository repository;
 
+  @Override
   @BeforeEach
-  void setUp() {
-    insertDocuments();
-
+  public void setUp() {
+    super.setUp();
     repository = new OperatorRepository(getMongoTemplate());
   }
 
@@ -31,27 +28,4 @@ class OperatorRepositoryTest extends AbstractSpringTest {
     assertThat(operators).containsOnly(OPERATOR);
   }
 
-  private void insertDocuments() {
-    final var documents = IntStream.rangeClosed(1, 10).mapToObj(this::createDocument).collect(toList());
-    getMongoTemplate().insert(documents, "gps-data");
-  }
-
-  private Document createDocument(final int documentNumber) {
-    return new Document()
-        .append("timestamp", 1352246396000000L + documentNumber)
-        .append("lineId", documentNumber)
-        .append("direction", "1")
-        .append("journeyPatternId", documentNumber)
-        .append("timeFrame", now().plusDays(documentNumber))
-        .append("vehicleJourneyId", documentNumber)
-        .append("operator", OPERATOR)
-        .append("congestion", false)
-        .append("longitude", -6.258584)
-        .append("latitude", 53.340099)
-        .append("delay", 0)
-        .append("blockId", documentNumber)
-        .append("vehicleId", documentNumber)
-        .append("stopId", documentNumber)
-        .append("atStop", false);
-  }
 }
