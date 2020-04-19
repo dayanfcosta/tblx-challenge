45248176675365eac70a4fb936286abdac1498fc
Dayan Costa <dayan.costa@trustly.com>
2020-04-19 21:17:00 +0100

    minor fixes
    Former-commit-id: 32e43e5b76d95e46dbd824e5944e2b591f757c74

--- src/test/java/com/dayanfcosta/tblx/challenge/AbstractResourceTest.java
+++ src/test/java/com/dayanfcosta/tblx/challenge/AbstractResourceTest.java
@@ -14,9 +14,11 @@ import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWeb
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 @AutoConfigureMockMvc
+@ActiveProfiles("test")
 @AutoConfigureWebTestClient
 @ExtendWith(SpringExtension.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
